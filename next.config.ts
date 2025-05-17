55dbfd07f08a0e4c03cda0d5a8ca6ede8232c0af
Carl Antoine (Ant Designs LLC) <carlantoine14@gmail.com>
2025-05-16 22:02:35 -0400

    config

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+const isGithubPages = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  assetPrefix: isGithubPages ? '/your-repo-name/' : '',
+  basePath: isGithubPages ? '/your-repo-name' : '',
+  trailingSlash: true, // Helps with static export
+};
+
+
 export default nextConfig;
